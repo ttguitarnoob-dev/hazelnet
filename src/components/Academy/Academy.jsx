ac301311fa375fa37a5a22269159712e306421aa
ttguitarnoob-dev <ttguitarnoob@gmail.com>
2023-07-11 15:08:43 -0500

    academy page buttons set up

--- src/components/Academy/Academy.jsx
+++ src/components/Academy/Academy.jsx
@@ -2,11 +2,17 @@ export default function Academy() {
     console.log("plzwork")
 
     return <div className="academy-page">
-        <h2>KittyCottage Academy</h2>
-        <ul>
-            <li>
-                <a href="/academy/journal">Learning Journal</a>
-            </li>
-        </ul>
+        <div className="page-content">
+            <h2>KittyCottage Academy</h2>
+            <div className="main-buttons">
+                <ul>
+                    <li><a href="/academy/journal"><button>School Journal</button></a></li>
+                    <li><a href="/poke"><button>Pokedex!</button></a></li>
+                    <li><a href="/videos"><button>Videos!</button></a></li>
+                    <li><a href="#"><button>Prodigy!</button></a></li>
+                    <li><a href="/academy"><button>School!</button></a></li>
+                </ul>
+            </div>
+        </div>
     </div>
 }
