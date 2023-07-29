b992fa62b26c6b71b56de4cb6f6b18784a2b9da9
ttguitarnoob-dev <ttguitarnoob@gmail.com>
2023-07-29 15:24:52 -0600

    verse of the day functionality and styling finished

--- src/components/Hazel.jsx
+++ src/components/Hazel.jsx
@@ -1,9 +1,15 @@
 
+import { useState, useEffect } from "react"
 
-export default function Hazel(props) {
+
+
+export default function Hazel() {
     var currentHour = new Date().getHours()
-    const dailyVerse = props
-   
+
+
+    //State
+    let [dailyVerse, setDailyVerse] = useState()
+
     var prodigy = ""
 
     if (currentHour < 10) {
@@ -11,6 +17,31 @@ export default function Hazel(props) {
     } else {
         prodigy = "https://sso.prodigygame.com/game/login?rid=daa00d0b-85de-4ec2-ae43-031f7232101d"
     }
+
+    //Verse Fetch
+    async function fetchVerse() {
+
+        try {
+            const url = "https://labs.bible.org/api/?passage=votd&type=json"
+            const options = {
+                method: "GET"
+            }
+            const response = await fetch(url, options)
+            const data = await response.json()
+            // const data = await response.text()
+            console.log('versess', data)
+            setDailyVerse(data)
+        } catch (err) {
+            console.log('something broke when fetching a verse', err)
+        }
+    }
+
+    //fetch verse function call
+    useEffect(() => {
+        fetchVerse()
+    }, [])
+
+    console.log('verses', dailyVerse)
 
     // console.log('progeny', dailyVerse.dailyVerse[0].text)
     return <div className="hazel-page">
@@ -23,14 +54,25 @@ export default function Hazel(props) {
                 <li><a href="/academy"><button>School!</button></a></li>
             </ul>
         </div>
+
         <div className="daily-verse">
             {/* <h2>Bible Verse of the Day!</h2> */}
-            <iframe src="https://www.bible.com/en-GB/verse-of-the-day"></iframe>
+            {/* <iframe src="https://www.bible.com/en-GB/verse-of-the-day"></iframe> */}
             {/* <p>{dailyVerse.dailyVerse}</p> */}
             {/* <a>
                 <h3>{dailyVerse.dailyVerse[0].bookname} {dailyVerse.dailyVerse[0].chapter}:{dailyVerse.dailyVerse[0].verse}</h3>
                 <p>{dailyVerse.dailyVerse[0].text}</p>
             </a> */}
+            <h2>Bible Verses of the Day!</h2>
+            <ul>
+                {dailyVerse && dailyVerse.map((verse, index) => (
+                    <li key={index}>
+                        <h3>{verse.bookname} {verse.chapter}:{verse.verse}</h3>
+                        <p>{verse.text}</p>
+                    </li>
+                ))}
+            </ul>
         </div>
+
     </div>
 }
