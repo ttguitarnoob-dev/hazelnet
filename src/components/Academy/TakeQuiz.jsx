2ab38303d6a329f57d278e578f3093ad9af34f24
ttguitarnoob-dev <ttguitarnoob@gmail.com>
2023-10-16 18:30:52 -0500

    selecting choices and clicking next will update state with choice, determine if the answer is correct, and then change boolean for correct

--- src/components/Academy/TakeQuiz.jsx
+++ src/components/Academy/TakeQuiz.jsx
@@ -1,12 +1,14 @@
 import { useEffect, useState } from "react";
 import { handleFetch } from "../utils/fetch";
 import { useParams } from "react-router-dom";
+import { act } from "react-dom/test-utils";
 
 export default function TakeQuiz() {
 
     const [quiz, setQuiz] = useState()
     const { id } = useParams()
     const [activeQuestion, setActiveQuestion] = useState(0)
+    const [selected, setSelected] = useState(0)
     const choiceLetters = {
         0: "A",
         1: "B",
@@ -29,9 +31,21 @@ export default function TakeQuiz() {
         getData()
     }, [])
 
-    function handleNext() {
+    function handleNext(e) {
+        e.preventDefault()
+
+        const modifiedQuestion = quiz.questions[activeQuestion]
+
+        modifiedQuestion.choice = parseInt(selected)
+        if (modifiedQuestion.choice === modifiedQuestion.answer){
+            console.log(modifiedQuestion.choice, "is the same as", modifiedQuestion.answer, "so it's correct")
+            modifiedQuestion.correct = true
+        }
         if (activeQuestion < quiz.questions.length - 1) {
+            console.log('quiiz', quiz.questions[activeQuestion].choice)
+
             setActiveQuestion((prev) => prev + 1)
+
         }
     }
 
@@ -42,27 +56,33 @@ export default function TakeQuiz() {
         }
     }
 
+    function handleSelect(e) {
+        setSelected(e.target.value)
+    }
 
 
+    console.log('slelected', quiz)
 
     return (
         <section className="container">
             {quiz && <div>
                 <h1>{quiz.title}</h1>
-                <p>Question: {quiz.questions[activeQuestion].question}</p>
-                <form>
+                <h2>Question {activeQuestion + 1}/{quiz.questions.length}:</h2>
+                <p>choice: {quiz.questions[activeQuestion].choice}</p>
+                <p>{quiz.questions[activeQuestion].question}</p>
+                <form onSubmit={handleNext}>
                     <ol>
                         {quiz.questions[activeQuestion].choices && quiz.questions[activeQuestion].choices.map((oneChoice, index) => (
                             <div key={index}>
-                                <input type="radio" name="choices" id="choice1" key={index} value={index}/>
-                                <label for="choice1">{choiceLetters[index]}: {oneChoice}</label>
+                                <input type="radio" name="choices" id="choice1" key={index} value={index} onChange={handleSelect} />
+                                <label htmlFor="choice1">{choiceLetters[index]}: {oneChoice}</label>
 
                             </div>
                         ))}
                     </ol>
-
+                    <button>Next</button>
                 </form>
-                <button onClick={handleNext}>Next</button>
+
                 <button onClick={handleBack}>Back</button>
             </div>}
         </section>
