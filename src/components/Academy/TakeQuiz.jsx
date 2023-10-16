711e972ebf1f48a5bb68a68bf0d562bb51ac50ea
ttguitarnoob-dev <ttguitarnoob@gmail.com>
2023-10-16 17:46:01 -0500

    radio buttons populate based on choices from api

--- src/components/Academy/TakeQuiz.jsx
+++ src/components/Academy/TakeQuiz.jsx
@@ -6,6 +6,13 @@ export default function TakeQuiz() {
 
     const [quiz, setQuiz] = useState()
     const { id } = useParams()
+    const [activeQuestion, setActiveQuestion] = useState(0)
+    const choiceLetters = {
+        0: "A",
+        1: "B",
+        2: "C",
+        3: "D",
+    }
 
     async function getData() {
         const endpoint = `quizzes/${id}`
@@ -22,14 +29,41 @@ export default function TakeQuiz() {
         getData()
     }, [])
 
+    function handleNext() {
+        if (activeQuestion < quiz.questions.length - 1) {
+            setActiveQuestion((prev) => prev + 1)
+        }
+    }
+
+    function handleBack() {
+        console.log('backclicked')
+        if (activeQuestion > 0) {
+            setActiveQuestion((prev) => prev - 1)
+        }
+    }
+
 
 
 
     return (
-        <section>
+        <section className="container">
             {quiz && <div>
-                <h1>Quizz {quiz.title}</h1>
-                <p>Question: {quiz.questions[0].question}</p>
+                <h1>{quiz.title}</h1>
+                <p>Question: {quiz.questions[activeQuestion].question}</p>
+                <form>
+                    <ol>
+                        {quiz.questions[activeQuestion].choices && quiz.questions[activeQuestion].choices.map((oneChoice, index) => (
+                            <div key={index}>
+                                <input type="radio" name="choices" id="choice1" key={index} value={index}/>
+                                <label for="choice1">{choiceLetters[index]}: {oneChoice}</label>
+
+                            </div>
+                        ))}
+                    </ol>
+
+                </form>
+                <button onClick={handleNext}>Next</button>
+                <button onClick={handleBack}>Back</button>
             </div>}
         </section>
     )
